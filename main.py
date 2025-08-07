21de0d71665fd01b25a3b4dda5d41e6a89488f2b
vadim <vadumcimbalist@gmail.com>
2025-08-07 19:02:32 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
 import time
 import logging
+
+from src.scraper.scraper import Scraper
 from utils.logger_setup import setup_logging
 from config.config import load_config
-from utils.driver import create_driver
-from scraper.scraper import scrape_page
-from utils.storage import save_to_json
+from utils.output_writer import OutputWriter
 
 def run():
     setup_logging()
-    config = load_config()
+    config = load_config() #TODO: model for config
     interval = config.get("interval", 300)
     proxies = config.get("proxies", [])
 
-    driver = None
+    scraper = Scraper(proxies)
+    output_writer = OutputWriter("data")
+
     try:
         while True:
             logging.info("Web scraping started...")
-            if not driver:
-                driver = create_driver(proxies)
-                if not driver:
-                    logging.error("Failed to initialize WebDriver. Retrying in 60 seconds.")
-                    time.sleep(60)
-                    continue
 
-            data = scrape_page(driver)
-            save_to_json(data)
+            scraping_result = scraper.scrape_page()
+            output_writer.save_to_json(scraping_result)
 
             logging.info(f"Web scraping process completed. Sleeping for {interval} seconds.")
             time.sleep(interval)
     except KeyboardInterrupt:
-        logging.info("Scraper manually stopped. Cleaning up...")
+        logging.info("Scraper was stopped manually. Cleaning up...")
     finally:
-        if driver:
-            driver.quit()
+        scraper.stop()
         logging.info("Resources cleaned up successfully. Exiting.")
 
 if __name__ == '__main__':
